8cbe86ab13da8f9920b1ff10396f0ecad29890ec
Aayush Arya <aayush10738@gmail.com>
2025-10-02 02:41:17 +0530

    changed gallery in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,21 @@ import Landing from "./Landing";
 import PreTechnika from "./PreTechnika";
 import Footer from "./Footer";
 import { Slider } from "../components/Slider";
+import CircularGallery from '../components/CircularGallery'
 
 
 const Home = () => {
   return (
     <>
-      <Landing/>
-      <PreTechnika/>
-      <Slider />
-      <Footer/>
+      <Landing />
+      <div style={{ width: '98vw', height: '100vh' }}>
+        <PreTechnika />
+      </div>
+      <div className="mb-15" style={{ height: '600px', position: 'relative' }}>
+        <CircularGallery bend={3} textColor="#ffffff" borderRadius={0.05} scrollEase={0.02} />
+      </div>
+      {/* <Slider /> */}
+      <Footer />
     </>
   )
 }
