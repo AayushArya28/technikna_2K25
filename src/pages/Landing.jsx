a980b03825ab0e88218a89f14d6b15a7c2651f9b
Aayush Arya <aayush10738@gmail.com>
2025-10-03 00:38:16 +0530

    feat: Added Register Button in Homepage

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -183,6 +183,35 @@ const Landing = () => {
               2025
             </span>
           </h2>
+          {/* REGISTER Button */}
+        <Link
+          to="/login"
+          className="absolute bottom-7 right-6 flex items-center justify-center group"
+        >
+          {/* Mobile Image */}
+          <div className="relative block md:hidden w-56">
+            <img
+              src="https://www.bits-oasis.org/svgs/landing/mobileRegisterBtn.svg"
+              alt="Register"
+              className="w-full transition duration-300 group-hover:brightness-75"
+            />
+            <span className="absolute inset-0 flex items-center justify-center text-yellow-400 font-bold text-lg pointer-events-none">
+              REGISTER
+            </span>
+          </div>
+
+          {/* Desktop Image */}
+          <div className="relative hidden md:block w-96">
+            <img
+              src="https://www.bits-oasis.org/svgs/landing/registerBtn.svg"
+              alt="Register"
+              className="w-full transition duration-300 group-hover:brightness-75"
+            />
+            <span className="mb-4 absolute inset-0 flex items-center justify-center text-white font-bold text-4xl pointer-events-none">
+              REGISTER
+            </span>
+          </div>
+        </Link>
         </div>
       </div>
 
