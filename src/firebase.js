fb720fd7f81b99b21b137b2629d23fd5e04734e4
Ashutosh Kumar <89670760+Ashutosh9470@users.noreply.github.com>
2025-10-03 09:09:23 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,11 +6,12 @@ const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET, // ✅ should be "technika25-5db19.appspot.com"
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
