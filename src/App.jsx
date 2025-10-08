2a029030169e5b05031d4dbf8373248f39f0fad2
ikrishagarwal <68506145+ikrishagarwal@users.noreply.github.com>
2025-10-08 23:32:26 +0530

    fix: nav bar bugs

--- src/App.jsx
+++ src/App.jsx
@@ -15,12 +15,11 @@ import { Analytics } from "@vercel/analytics/react";
 import TransitionComponent from "./components/Transition.jsx";
 import { TransitionProvider } from "./context/transition.jsx";
 
-
 function App() {
   const routes = [
     { path: "/", Component: Home },
     { path: "/events", Component: Events },
-    { path: "/merchandise", Component : Merchandise },
+    { path: "/merchandise", Component: Merchandise },
     { path: "/core", Component: Core },
     { path: "/workshop", Component: Workshop },
     { path: "/login", Component: Login },
@@ -46,7 +45,7 @@ function App() {
                   element={
                     <TransitionComponent>
                       <>
-                        <div className="fixed top-0 left-0 w-full h-full pointer-events-none z-[100] overflow-hidden">
+                        <div className="fixed top-0 left-0 w-full h-full pointer-events-none z-[9999] overflow-hidden">
                           <img
                             src="/images/left-door.jpg"
                             className="absolute top-0 left-0 w-auto min-w-[50vw] min-h-screen h-auto object-right door left-door translate-x-[-100%]"
