1fb54820faa7fd81a1a3cbcb5dea8664ec8e6a82
Aayush Arya <aayush10738@gmail.com>
2025-12-13 22:19:47 +0530

    Update developer role to 'Full Stack'

--- src/pages/Devs.jsx
+++ src/pages/Devs.jsx
@@ -275,7 +275,7 @@ const DevsPage = () => {
     {
       id: 1,
       name: "Aayush Arya",
-      role: "ye bagal wala mc kon hai",
+      role: "Full Stack",
       branch: "ECE",
       year: "2nd Year",
       image: "images/devs/aayush.png",
