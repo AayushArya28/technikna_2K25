68986e9d7b5163cdeecdf106dac5ea90860bea3d
Aayush Arya <aayush10738@gmail.com>
2025-12-19 14:25:28 +0530

    Temp site down

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,13 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+  {/* <App /> */}
+    <div className="pt-25 min-h-screen flex items-center justify-center bg-black">
+      <img
+        src="/images/coming-soon.jpg"
+        alt="Coming Soon"
+        className="max-w-xs sm:max-w-sm md:max-w-md opacity-90"
+      />
+    </div>
   </StrictMode>,
 )
